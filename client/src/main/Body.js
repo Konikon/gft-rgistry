3b597fcae4d968ca309edaa637de3787056282e8
Mak Karmarkar <mkarmarkar@gmail.com>
2017-11-07 15:11:01 -0700

    initial commit for gift registry project

---
+++ client/src/main/Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Body(){
+  return(
+    <div>
+      Hi From Body!
+    </div>
+  )
+  /* body container Switch in submit button to graph component */
+}
+
+
+export default Body;
